2d9fc66d3a5468ad55396bb285a3f14958cd59c8
Nic Mayer <nicmayer42@gmail.com>
2019-08-15 17:19:49 +0100

    Tidying

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ navIcon[0].addEventListener('click', function(){
 //Move avatar up to be level with the top of the navigation
 const nav = document.getElementsByClassName('navigation');
 const navHeight = nav[0].offsetHeight;
-/*const avatar = document.getElementById('avatar');
-avatar.style.top = navHeight * -1 + 'px';*/
 //And resize it to fit the navHeight
 let avatarHeight = navHeight - 2; //to account for border
 document.getElementById('avatar-img').style.height = avatarHeight + 'px';
